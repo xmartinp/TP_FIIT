8416f634fa95fa1cf6c8666a94b8c1ee118df8eb
AhmedAlqnatri <43376654+AhmedAlqnatri@users.noreply.github.com>
2022-04-07 11:31:12 +0200

    Custom LTE scenario with fixing tcl file to be in the grid

--- P2/simulacia.cc
+++ P2/simulacia.cc
@@ -1,4 +1,4 @@
-#include "wifi-setup.h"
+
 #include "ns3/network-module.h"
 #include "ns3/core-module.h"
 #include "ns3/mobility-module.h"
@@ -23,18 +23,15 @@
 #include "ns3/yans-wifi-channel.h"
 #include "ns3/mobility-model.h"
 #include "ns3/packet-socket-helper.h"
-#include "ns3/packet-sink.h"
-#include "ns3/packet-sink-helper.h"
 #include "ns3/packet-socket-address.h"
 #include "ns3/athstats-helper.h"
 #include "ns3/netanim-module.h"
 #include "ns3/wave-module.h"
+#include <ns3/lte-module.h>
 #include <vector>
 #include <limits>
 
 using namespace ns3;
-
-// test
 
 // Custom function to print current position of the specified Node, to be used with Simulator::Schedule() function
 // Example usage: Simulator::Schedule(Seconds(0), &printCurrentPosition, nodes.Get(1)->GetObject<MobilityModel>());
@@ -51,10 +48,10 @@ Ptr<NetDevice> findClosestNetDevice(Ptr<NetDevice> currentDevice, NetDeviceConta
   Ptr<NetDevice> closestFoundDevice;
   Vector currentDevicePosition = currentDevice->GetNode() -> GetObject<MobilityModel>() -> GetPosition();
   double distance = 0;
-  
+
   for(NetDeviceContainer::Iterator i = possibleDevices.Begin(); i != possibleDevices.End(); ++i){
       Vector position = (*i) -> GetNode() -> GetObject<MobilityModel>() -> GetPosition ();
-      
+
       distance = (currentDevicePosition - position).GetLength ();
 
       if (distance < minimalDistanceFound)
@@ -71,7 +68,6 @@ Ptr<NetDevice> findClosestNetDevice(Ptr<NetDevice> currentDevice, NetDeviceConta
 void printTime(){
     std::cout << "a" << std::endl;
 }
-
 
 NS_LOG_COMPONENT_DEFINE("CustomLoggingExample");
 
@@ -90,11 +86,141 @@ int main (int argc, char *argv[])
     Ns2NodeUtility ns2_utility (mobility_file);
     uint32_t nnodes = ns2_utility.GetNNodes();
     double sim_time = ns2_utility.GetSimulationTime();
-    NodeContainer nodes;
-    nodes.Create (nnodes);
-
+    
     Ns2MobilityHelper sumo_trace (mobility_file);
     sumo_trace.Install();
+
+    std::cout<<"vypis1\n";
+    ////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////// Install LTE ///////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
+    Ptr<PointToPointEpcHelper> epcHelper = CreateObject<PointToPointEpcHelper> ();
+    lteHelper->SetEpcHelper (epcHelper); // Set the EpcHelper to be used to setup the EPC network in conjunction with the setup of the LTE radio access network.
+    lteHelper->SetSchedulerType ("ns3::RrFfMacScheduler");
+    lteHelper->SetHandoverAlgorithmType ("ns3::NoOpHandoverAlgorithm"); // disable automatic handover
+
+    Ptr<Node> pgw = epcHelper->GetPgwNode (); // a pointer to the node implementing PGW functionality. 
+
+    // Create a NodeContainer with exactly one node which has been previously instantiated as a single RemoteHost .
+    NodeContainer remoteHostContainer;
+    remoteHostContainer.Create (1);
+    Ptr<Node> remoteHost = remoteHostContainer.Get (0);
+    InternetStackHelper internet;
+    internet.Install (remoteHostContainer);
+      // Create the Internet
+    PointToPointHelper p2ph; // Create a PointToPointHelper to make life easier when creating point to point networks.
+    p2ph.SetDeviceAttribute ("DataRate", DataRateValue (DataRate ("100Gb/s")));
+    p2ph.SetDeviceAttribute ("Mtu", UintegerValue (1500));
+    p2ph.SetChannelAttribute ("Delay", TimeValue (Seconds (0.010)));
+    NetDeviceContainer internetDevices = p2ph.Install (pgw, remoteHost);
+    Ipv4AddressHelper ipv4h;
+    ipv4h.SetBase ("1.0.0.0", "255.0.0.0");
+    Ipv4InterfaceContainer internetIpIfaces = ipv4h.Assign (internetDevices);
+    Ipv4Address remoteHostAddr = internetIpIfaces.GetAddress (1);
+      // Routing of the Internet Host (towards the LTE network)
+    Ipv4StaticRoutingHelper ipv4RoutingHelper;
+    Ptr<Ipv4StaticRouting> remoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting (remoteHost->GetObject<Ipv4> ());
+    // interface 0 is localhost, 1 is the p2p device
+    remoteHostStaticRouting->AddNetworkRouteTo (Ipv4Address ("7.0.0.0"), Ipv4Mask ("255.0.0.0"), 1);
+
+    NodeContainer enbNodes;
+    enbNodes.Create (1);
+    NodeContainer ueNodes;
+    ueNodes.Create (nnodes);
+    MobilityHelper mobility;
+
+    mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
+                            "Bounds", RectangleValue (Rectangle (-100, 100, -50, 100)));
+    mobility.Install (ueNodes);
+    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+    mobility.Install (enbNodes);
+    //mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+     // Install LTE Devices in eNB and UEs
+    NetDeviceContainer enbLteDevs = lteHelper->InstallEnbDevice (enbNodes);
+    NetDeviceContainer ueLteDevs = lteHelper->InstallUeDevice (ueNodes);
+
+    // Install the IP stack on the UEs
+    internet.Install (ueNodes);
+    Ipv4InterfaceContainer ueIpIfaces;
+    ueIpIfaces = epcHelper->AssignUeIpv4Address (NetDeviceContainer (ueLteDevs));
+
+    lteHelper->AttachToClosestEnb (ueLteDevs, enbLteDevs);
+
+
+    NS_LOG_LOGIC ("setting up applications");
+    // Install and start applications on UEs and remote host
+    uint16_t dlPort = 10000;
+    uint16_t ulPort = 20000;
+
+    // randomize a bit start times to avoid simulation artifacts
+    // (e.g., buffer overflows due to packet transmissions happening
+    // exactly at the same time)
+    Ptr<UniformRandomVariable> startTimeSeconds = CreateObject<UniformRandomVariable> ();
+    startTimeSeconds->SetAttribute ("Min", DoubleValue (0));
+    startTimeSeconds->SetAttribute ("Max", DoubleValue (0.010)); 
+
+    for (uint32_t u = 0; u < nnodes; ++u)
+    {
+      Ptr<Node> ue = ueNodes.Get (u);
+      // Set the default gateway for the UE
+      Ptr<Ipv4StaticRouting> ueStaticRouting = ipv4RoutingHelper.GetStaticRouting (ue->GetObject<Ipv4> ());
+      ueStaticRouting->SetDefaultRoute (epcHelper->GetUeDefaultGatewayAddress (), 1);
+
+      //for (uint32_t b = 0; b < numBearersPerUe; ++b)
+        {
+          ++dlPort;
+          ++ulPort;
+
+          ApplicationContainer clientApps;
+          ApplicationContainer serverApps;
+
+          NS_LOG_LOGIC ("installing UDP DL app for UE " << u);
+          UdpClientHelper dlClientHelper (ueIpIfaces.GetAddress (u), dlPort);
+          clientApps.Add (dlClientHelper.Install (remoteHost));
+          PacketSinkHelper dlPacketSinkHelper ("ns3::UdpSocketFactory",
+                                               InetSocketAddress (Ipv4Address::GetAny (), dlPort));
+          serverApps.Add (dlPacketSinkHelper.Install (ue));
+
+          NS_LOG_LOGIC ("installing UDP UL app for UE " << u);
+          UdpClientHelper ulClientHelper (remoteHostAddr, ulPort);
+          clientApps.Add (ulClientHelper.Install (ue));
+          PacketSinkHelper ulPacketSinkHelper ("ns3::UdpSocketFactory",
+                                               InetSocketAddress (Ipv4Address::GetAny (), ulPort));
+          serverApps.Add (ulPacketSinkHelper.Install (remoteHost));
+
+          Ptr<EpcTft> tft = Create<EpcTft> ();
+          EpcTft::PacketFilter dlpf;
+          dlpf.localPortStart = dlPort;
+          dlpf.localPortEnd = dlPort;
+          tft->Add (dlpf);
+          EpcTft::PacketFilter ulpf;
+          ulpf.remotePortStart = ulPort;
+          ulpf.remotePortEnd = ulPort;
+          tft->Add (ulpf);
+          EpsBearer bearer (EpsBearer::GBR_CONV_VOICE);
+          lteHelper->ActivateDedicatedEpsBearer (ueLteDevs.Get (u), bearer, tft);
+
+          Time startTime = Seconds (startTimeSeconds->GetValue ());
+          serverApps.Start (startTime);
+          clientApps.Start (startTime);
+
+        } 
+    }
+     // Add X2 inteface
+    lteHelper->AddX2Interface (enbNodes);
+
+    // Enable tracing
+    lteHelper->EnablePhyTraces ();
+    lteHelper->EnableMacTraces ();
+    lteHelper->EnableRlcTraces ();
+    lteHelper->EnablePdcpTraces ();
+    
+    Ptr<RadioBearerStatsCalculator> rlcStats = lteHelper->GetRlcStats ();
+    rlcStats->SetAttribute ("EpochDuration", TimeValue (Seconds (0.02)));
+    Ptr<RadioBearerStatsCalculator> pdcpStats = lteHelper->GetPdcpStats ();
+    pdcpStats->SetAttribute ("EpochDuration", TimeValue (Seconds (0.02)));
 
     ////////////////////////////////////////////////////////////////////////////////////
     //// Install the required communication methods one by one
@@ -104,9 +230,7 @@ int main (int argc, char *argv[])
     //// Install AdHoc WiFi
     ////////////////////////////////////////////////////////////////////////////////////
 
-    
-
-    std::cout<<"vypis1\n";
+    /*
     Packet::EnablePrinting ();
     PacketSocketHelper packetSocket;
     packetSocket.Install(nodes);
@@ -147,22 +271,10 @@ int main (int argc, char *argv[])
     socket.SetPhysicalAddress (devices.Get (0)->GetAddress ());
     socket.SetProtocol (1);
     std::cout<<"vypis4\n";
-
-    /* Packet Sink pokus
-    std::cout<<"Packet sink install:";
-    
-    Ptr<Node> testWifiNode = nodes.Get(1);
-    PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), 9));
-    ApplicationContainer sinkApp = sinkHelper.Install (testWifiNode);
-    sinkHelper.Install (testWifiNode);
-    //sink = StaticCast<PacketSink> (sinkApp.Get (0));
-    */
-    
     // trieda na vytváranie komunikácie, trieda vytvára pakety
     OnOffHelper onoff ("ns3::PacketSocketFactory", Address (socket));
     onoff.SetConstantRate (DataRate ("500kb/s"));
-    onoff.SetAttribute("PacketSize", UintegerValue(2000));
-    //onoff.SetAttribute("PacketPayload", "asdasdasfasfsdfsdf");
+    //    onoff.SetAttribute("PacketSize", UintegerValue(2000));
     std::cout<<"vypis5\n";
     // nastavenie posielania paketov pomocou onoff.
     ApplicationContainer apps = onoff.Install (nodes.Get (0));
@@ -170,18 +282,18 @@ int main (int argc, char *argv[])
     apps.Stop (Seconds (70.0));
     std::cout<<"vypis6\n";
     Ptr<NetDevice> d0 = devices.Get(0);
-    
-    
+
+
     //Simulator::Schedule ( Seconds(1) , &findClosestNetDevice, d0, &devices);
 
-    
 
+*/
     /////////////////////////////////////////////////////////////////////////////////
     /// WAVE
     /////////////////////////////////////////////////////////////////////////////////
     /// Setup
     /*
-    
+
 
 
     YansWifiChannelHelper waveChannel = YansWifiChannelHelper::Default ();
@@ -276,11 +388,9 @@ int main (int argc, char *argv[])
     */
     ///////////////////////////////////////////////////////////
 
-    AthstatsHelper athstats;
-    athstats.EnableAthstats ("athstats-sta", nodes);
     Simulator::Stop(Seconds (sim_time));
     std::cout<<"vypis7\n";
-    //AnimationInterface anim("filename.xml");
+    AnimationInterface anim("/usr/ns-allinone-3.30/ns-3.30/scratch/P2/LTE.xml");
 
 
 
@@ -290,5 +400,5 @@ int main (int argc, char *argv[])
     Simulator::Destroy();
 
     std::cout << "End of Program" << std::endl;
-
+    return 0;
 }
